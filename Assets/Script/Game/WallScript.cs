cda5f1965fa94dfa2cd53c77b0094195c512374a
Natsume-Mahiro <natsume.mahiro.0528@gmail.com>
2023-12-31 23:59:37 +0900

    Update Wall
    壁のテクスチャ、スクリプトの変更

--- Assets/Script/Game/WallScript.cs
+++ Assets/Script/Game/WallScript.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class WallScript : MonoBehaviour
 {
     [SerializeField] Vector4 newClipNormalValue; // 新しい ClipNormal の値をインスペクターから設定
+    [SerializeField] Texture2D newTexture; // 新しいテクスチャをインスペクターから設定
 
     Renderer rend;
 
@@ -15,6 +16,18 @@ public class WallScript : MonoBehaviour
             if (mat != null)
             {
                 mat.SetVector("_ClipNormal", newClipNormalValue); // マテリアルの _ClipNormal を変更
+
+                if (newTexture != null)
+                {
+                    mat.mainTexture = newTexture; // テクスチャを変更
+
+                    // オブジェクトのスケールに合わせてテクスチャのタイリングを変更
+                    Vector2 newTiling = new Vector2(0, 0);
+                    if (newClipNormalValue.x != 0) newTiling = new Vector2(transform.localScale.z, transform.localScale.y);
+                    if (newClipNormalValue.y != 0) newTiling = new Vector2(transform.localScale.x, transform.localScale.z);
+                    if (newClipNormalValue.z != 0) newTiling = new Vector2(transform.localScale.x, transform.localScale.y);
+                    mat.mainTextureScale = newTiling;
+                }
             }
         }
     }
